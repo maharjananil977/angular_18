66c11562ab899cb3060dc6259a0a27af871c114b
maharjananil <maharjananil1@gmail.com>
2024-08-27 09:27:05 +0545

    structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  isLoggedIn: boolean = true;
+  userName: string = "Anil Maharjan"
 }
