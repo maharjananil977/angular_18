0471f2cabc96ab1cf1680723888fd7522a22571d
maharjananil <maharjananil1@gmail.com>
2024-08-27 11:05:00 +0545

    ng for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -6,11 +6,46 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,NgIf],
+  imports: [RouterOutlet,FormsModule,NgIf,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  isLoggedIn: boolean = true;
-  userName: string = "Anil Maharjan"
+ users: Array<string> = ['John','Sam','Smith','Raj']; 
+ usersObj: Array<any> = [
+  {
+    id:1,
+    name:'John',
+    age: 35
+  },
+  {
+    id:2,
+    name:'John',
+    age: 35
+  },
+  {
+    id:3,
+    name:'John',
+    age: 35
+  },
+  {
+    id:4,
+    name:'John',
+    age: 35
+  },
+  {
+    id:5,
+    name:'John',
+    age: 35
+  }
+ ]
+
+ addNewUser(){
+  this.usersObj.push({id:6,name:'test',age:33})
+ }
+ deleteUser(user :any){
+  let index = this.usersObj.indexOf(user)
+  console.log(index);
+  this.usersObj.splice(index, 1);
+ }
 }
