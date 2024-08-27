3b88439fb605c4d4c88a72e3d1f1d1dc1067a8ff
maharjananil <maharjananil1@gmail.com>
2024-08-27 10:30:48 +0545

    ng template and ng template outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -6,11 +6,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,NgIf],
+  imports: [RouterOutlet,FormsModule,NgIf,NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  isLoggedIn: boolean = true;
+  isLoggedIn: boolean = false;
   userName: string = "Anil Maharjan"
+  loginCount: number =0; 
 }
